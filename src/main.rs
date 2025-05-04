1ccf174d6e53b35357d1c18d2159b2a401c392f7
Sarah Allen <sarah@ultrasaurus.com>
2025-05-04 10:41:24 -0700

    live reload works!

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,35 @@
 use axum::{
-    Router, Extension,
+    Router, 
+    routing::any_service,
 };
-use std::net::SocketAddr;
+use notify::Watcher;
+use std::{net::SocketAddr, path::Path};
 use tokio::net::TcpListener;
 use tower_livereload::LiveReloadLayer;
 use tower_http::services::{ServeDir,ServeFile};
 
-#[derive(Clone)]
-struct Config {
-    port: u16
-}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let config = Config {
-        port: 3030
-    };
+    let addr: SocketAddr = SocketAddr::from(([127, 0, 0, 1], 3030));
 
-    let addr: SocketAddr = SocketAddr::from(([127, 0, 0, 1], config.port));
-
-    let serve_dir = ServeDir::new("website"); //.not_found_service(ServeFile::new("website/index.html"));
+    let livereload = LiveReloadLayer::new();
+    let reloader = livereload.reloader();
 
     let app = Router::new()
-        .layer(LiveReloadLayer::new())
-        .layer(Extension(config))
-        .fallback_service(serve_dir);
+        .route("/{*key}", any_service(ServeDir::new("website")))
+        .route("/", any_service(ServeFile::new("website/index.html")))
+        .layer(livereload);
+
+    let mut watcher = notify::recommended_watcher(move |_| reloader.reload())?;
+    watcher.watch(Path::new("website"), notify::RecursiveMode::Recursive)?;
+
     
     let listener = TcpListener::bind(addr).await?;
 
     axum::serve(
         listener,
-        app.into_make_service_with_connect_info::<SocketAddr>(),
+        app,
     ).await?;
 
     Ok(())
